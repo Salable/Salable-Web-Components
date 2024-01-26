0ea73eb2249ffc6ed14c7429dc15ae0765b88420
Sean Cooper <65615704+adapta-sean@users.noreply.github.com>
2024-01-26 03:17:32 +0000

    fix: add theme examples (#70)

--- packages/react-client/src/components/salable-checkout/salable-checkout-demo.tsx
+++ packages/react-client/src/components/salable-checkout/salable-checkout-demo.tsx
@@ -1,14 +1,30 @@
 import { SalableCheckout } from "@salable/react-library";
 import {API_KEY} from "../../constants.ts";
+import styles from "../../theme.module.css";
 
 export default function SalableCheckoutDemo() {
     return (
-        <SalableCheckout
-            apiKey={API_KEY}
-            planUuid="351eefac-9b21-4299-8cde-302249d6fb1e"
-            member='example-member-123'
-            granteeId='example-grantee-123'
-            successUrl="https://www.google.com"
-        />
+        <>
+            <div>
+                <SalableCheckout
+                    apiKey={API_KEY}
+                    planUuid="351eefac-9b21-4299-8cde-302249d6fb1e"
+                    member='example-member-123'
+                    granteeId='example-grantee-123'
+                    successUrl="https://www.google.com"
+                />
+            </div>
+            <div className={styles.theme}>
+                <SalableCheckout
+                    apiKey={API_KEY}
+                    planUuid="351eefac-9b21-4299-8cde-302249d6fb1e"
+                    member='example-member-123'
+                    granteeId='example-grantee-123'
+                    successUrl="https://www.google.com"
+                />
+            </div>
+        </>
+
+
     )
 }
