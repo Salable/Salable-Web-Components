22078e10f93403c5f034b2b8100ddf67d7715bdc
Sean Cooper <scooper@adaptavist.com>
2024-01-26 03:04:09 +0000

    fix: add theme examples

--- packages/react-client/src/components/salable-invoices/salable-invoices-demo.tsx
+++ packages/react-client/src/components/salable-invoices/salable-invoices-demo.tsx
@@ -1,12 +1,24 @@
 import {SalableInvoices} from "@salable/react-library";
 import {API_KEY} from "../../constants.ts";
+import styles from "../../theme.module.css";
 
 export default function SalableInvoicesDemo() {
     return (
-        <SalableInvoices
-            apiKey={API_KEY}
-            subscriptionUuid="4ffe5998-77af-49b0-815b-66b8269c4abd"
-            limit={3}
-        />
+        <>
+            <div>
+                <SalableInvoices
+                    apiKey={API_KEY}
+                    subscriptionUuid="4ffe5998-77af-49b0-815b-66b8269c4abd"
+                    limit={3}
+                />
+            </div>
+            <div className={styles.theme}>
+                <SalableInvoices
+                    apiKey={API_KEY}
+                    subscriptionUuid="4ffe5998-77af-49b0-815b-66b8269c4abd"
+                    limit={3}
+                />
+            </div>
+        </>
     )
 }
