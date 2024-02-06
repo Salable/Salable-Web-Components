10c250945cfe43ff8953042c9b2bc8ae2e05d15b
Sean Cooper <scooper@adaptavist.com>
2024-02-06 17:55:04 +0000

    fix: pricing table react tests

--- packages/react-client/src/components/salable-pricing-table/salable-pricing-table.e2e.ts
+++ packages/react-client/src/components/salable-pricing-table/salable-pricing-table.e2e.ts
@@ -1,18 +1,19 @@
 import {test} from '@playwright/test';
 import {
     salablePricingTableTests,
-    setUpCustomPricingTableApi
+    setUpCustomPricingTableApi, setUpProductPricingTableApi
 } from "../../../../utilities/tests/salable-pricing-table-tests.ts";
 
 test.describe('salable-pricing-table React Client E2E Tests', () => {
     test('All Cases', async ({page}) => {
-        await setUpCustomPricingTableApi(page);
 
-        // Displays a custom pricing table and toggles between monthly/yearly intervals
+        // Displays a product pricing table and toggles between monthly/yearly intervals
+        await setUpCustomPricingTableApi(page);
         await page.goto('http://localhost:5173/test/salable-pricing-table');
         await salablePricingTableTests(page);
 
-        // Displays a product pricing table and toggles between monthly/yearly intervals
+        // Displays a custom pricing table and toggles between monthly/yearly intervals
+        await setUpProductPricingTableApi(page);
         await page.goto('http://localhost:5173/test/salable-pricing-table');
         await salablePricingTableTests(page);
     });
