5f724ed25136d2443505cccc62faf149486c60c3
Sean Cooper <scooper@adaptavist.com>
2024-02-06 16:31:13 +0000

    fix: try max workers 1

--- packages/react-client/src/components/salable-checkout/salable-checkout.e2e.ts
+++ packages/react-client/src/components/salable-checkout/salable-checkout.e2e.ts
@@ -1,5 +1,5 @@
-import { test } from '@playwright/test';
-import { mockCheckout } from '../../../../utilities/mock-data/checkout.mock'
+import {test} from '@playwright/test';
+import {mockCheckout} from '../../../../utilities/mock-data/checkout.mock'
 import {
     salableCheckoutInvalidEmailPrefillTest,
     salableCheckoutPaymentIntentTest,
@@ -9,34 +9,35 @@ import {
 } from "../../../../utilities/tests/salable-checkout-tests";
 
 test.describe('salable-checkout React Client E2E Tests', () => {
+    test.describe('Success Cases', () => {
+        test('Create payment intent form', async ({page}) => {
+            await setUpCheckoutFetch(page, mockCheckout());
+            await setUpPaymentIntent(page, {clientSecret: 'xxxxx'});
 
-    // test.describe('Fetch Success Cases', () => {
-    test('Create payment intent form', async ({ page }) => {
-        await setUpCheckoutFetch(page, mockCheckout());
-        await setUpPaymentIntent(page, { clientSecret: 'xxxxx' });
+            await page.goto('http://localhost:5173/test/salable-checkout');
 
-        await page.goto('http://localhost:5173/test/salable-checkout');
+            await salableCheckoutPaymentIntentTest(page);
+        });
 
-        await salableCheckoutPaymentIntentTest(page);
+        test('Prefill checkout with email', async ({page}) => {
+            await setUpCheckoutFetch(page, mockCheckout());
+            await setUpPaymentIntent(page, {clientSecret: 'xxxxx'});
+
+            await page.goto('http://localhost:5173/test/salable-checkout/valid-email');
+
+            await salableCheckoutPrefillWithEmailTest(page);
+        });
     });
-    // });
 
-    test('Prefill checkout with invalid email', async ({ page }) => {
-        await setUpCheckoutFetch(page, mockCheckout());
-        await setUpPaymentIntent(page, { clientSecret: 'xxxxx' });
+    test.describe('Failure Cases', () => {
+        test('Prefill checkout with invalid email', async ({page}) => {
+            await setUpCheckoutFetch(page, mockCheckout());
+            await setUpPaymentIntent(page, {clientSecret: 'xxxxx'});
 
-        await page.goto('http://localhost:5173/test/salable-checkout/invalid-email');
+            await page.goto('http://localhost:5173/test/salable-checkout/invalid-email');
 
-        await salableCheckoutInvalidEmailPrefillTest(page);
-    });
-
-    test('Prefill checkout with email', async ({ page }) => {
-        await setUpCheckoutFetch(page, mockCheckout());
-        await setUpPaymentIntent(page, { clientSecret: 'xxxxx' });
-
-        await page.goto('http://localhost:5173/test/salable-checkout/valid-email');
-
-        await salableCheckoutPrefillWithEmailTest(page);
+            await salableCheckoutInvalidEmailPrefillTest(page);
+        });
     });
 });
 
