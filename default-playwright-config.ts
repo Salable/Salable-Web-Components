32232a804b700d4c5f30b686d8e7571639a6055a
Sean Cooper <scooper@adaptavist.com>
2024-02-13 14:42:16 +0000

    fix: bump timeouts

--- default-playwright-config.ts
+++ default-playwright-config.ts
@@ -34,10 +34,10 @@ const projects = [
 ];
 
 export const defaultPlaywrightConfig: PlaywrightTestConfig = {
-    timeout: 5000,
+    timeout: 10000,
     globalTimeout: 300000,
     expect: {
-        timeout: 5000,
+        timeout: 10000,
         toMatchSnapshot: {
             threshold: 0.05,
             maxDiffPixelRatio: 0.05,
