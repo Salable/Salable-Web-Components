0ea40455d72494f4695eed4601b41ff3bb4cda4f
Sean Cooper <scooper@adaptavist.com>
2024-02-29 14:22:04 +0000

    fix: use test api key prefix for react checkout test component

--- packages/react-client/src/components/salable-checkout/salable-checkout-test.tsx
+++ packages/react-client/src/components/salable-checkout/salable-checkout-test.tsx
@@ -4,7 +4,7 @@ export default function SalableCheckoutTest({ email }: { email?: string }) {
     return (
         <SalableCheckout
             data-testid="component"
-            apiKey="xxxxx"
+            apiKey="test_xxxxx"
             planUuid="xxxxx"
             granteeId="xxxxx"
             member="xxxxx"
