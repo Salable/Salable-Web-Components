ee4655b9890c3c5078911db17467fb6ba4859669
Sean Cooper <scooper@adaptavist.com>
2024-02-29 17:36:06 +0000

    fix: clean up react checkout demos

--- packages/react-client/src/components/salable-checkout/salable-checkout-demo.tsx
+++ packages/react-client/src/components/salable-checkout/salable-checkout-demo.tsx
@@ -13,15 +13,6 @@ export default function SalableCheckoutDemo() {
                 granteeId='example-grantee-123'
                 successUrl="https://www.google.com"
             />
-            <h2>Default Themed</h2>
-            <SalableCheckout
-                className={styles.theme}
-                apiKey={API_KEY}
-                planUuid="351eefac-9b21-4299-8cde-302249d6fb1e"
-                member='example-member-123'
-                granteeId='example-grantee-123'
-                successUrl="https://www.google.com"
-            />
             <h2>With Valid Email</h2>
             <SalableCheckout
                 apiKey={API_KEY}
@@ -30,15 +21,6 @@ export default function SalableCheckoutDemo() {
                 granteeId='example-grantee-123'
                 successUrl="https://www.google.com"
                 email='johnnydoe@email.com'
-            />
-            <h2>With Invalid Email</h2>
-            <SalableCheckout
-                apiKey={API_KEY}
-                planUuid="351eefac-9b21-4299-8cde-302249d6fb1e"
-                member='example-member-123'
-                granteeId='example-grantee-123'
-                successUrl="https://www.google.com"
-                email='johnnydoe@email'
             />
              <h2>Theming</h2>
             <SalableCheckout
