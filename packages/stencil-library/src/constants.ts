e3ade6ad68eb19995d049d27738acc598d69ed5a
Sean Cooper <scooper@adaptavist.com>
2024-02-29 15:37:43 +0000

    fix: log envs

--- packages/stencil-library/src/constants.ts
+++ packages/stencil-library/src/constants.ts
@@ -1,3 +1,7 @@
 export const apiUrl = process.env.API_URL;
 export const stripePublicKey = process.env.STRIPE_PUBLIC_KEY;
 export const stripePublicKeyTestmode = process.env.STRIPE_PUBLIC_KEY_TESTMODE;
+
+console.log("apiUrl", apiUrl);
+console.log("stripePublicKey", stripePublicKey);
+console.log("stripePublicKeyTestmode", stripePublicKeyTestmode);
