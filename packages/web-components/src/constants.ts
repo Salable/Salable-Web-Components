196abdb0eae70b47cb35a8e628e9a22e4e08885c
Joshua Okoro <87143313+joshokoro@users.noreply.github.com>
2024-03-05 14:41:18 -0600

    chore: error handling for web component envs (#115)

--- packages/web-components/src/constants.ts
+++ packages/web-components/src/constants.ts
@@ -1,3 +1,18 @@
-export const apiUrl = process.env.API_URL;
-export const stripePublicKey = process.env.STRIPE_PUBLIC_KEY;
-export const stripePublicKeyTestmode = process.env.STRIPE_PUBLIC_KEY_TESTMODE;
+let apiUrl = ''
+let stripePublicKey = ''
+let stripePublicKeyTestmode = ''
+try {
+    apiUrl = process.env.API_URL;
+    stripePublicKey = process.env.STRIPE_PUBLIC_KEY;
+    stripePublicKeyTestmode = process.env.STRIPE_PUBLIC_KEY_TESTMODE;
+} catch (error) {
+    throw new Error("Setup your environment variables")
+}
+
+if (!Boolean(apiUrl) || !Boolean(stripePublicKey) || !Boolean(stripePublicKeyTestmode)) throw new Error("One or more of your environment variables is an empty string")
+
+export {
+    apiUrl,
+    stripePublicKey,
+    stripePublicKeyTestmode
+}
