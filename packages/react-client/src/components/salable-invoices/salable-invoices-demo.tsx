55a67617884f2d80e96acd72554eb018f291a650
Coner Murphy <conermurphy@users.noreply.github.com>
2024-10-08 10:01:37 +0100

    chore: update to use session token env

--- packages/react-client/src/components/salable-invoices/salable-invoices-demo.tsx
+++ packages/react-client/src/components/salable-invoices/salable-invoices-demo.tsx
@@ -1,20 +1,21 @@
 import { SalableInvoices } from "@salable/react-web-components";
 import styles from "../../theme.module.css";
+import { SESSION_TOKEN } from "../../constants";
 
 export default function SalableInvoicesDemo() {
   return (
     <div style={{ display: "flex", flexDirection: "column", gap: "40px" }}>
       <h2>Default</h2>
       <SalableInvoices
-        sessionToken="58062bb89881fa99379f9f1065404c5c"
-        subscriptionUuid="04c4bada-7133-4829-a27c-8e5b00558b9e"
+        sessionToken={SESSION_TOKEN}
+        subscriptionUuid="0145260e-99c3-49ce-88f8-9b458ab8f814"
         limit={3}
       />
       <h2>Default Themed</h2>
       <SalableInvoices
         className={styles.theme}
-        sessionToken="a0b037804148dcc70b6826b622ab9c1b"
-        subscriptionUuid="04c4bada-7133-4829-a27c-8e5b00558b9e"
+        sessionToken={SESSION_TOKEN}
+        subscriptionUuid="1c141ab7-ae1d-464d-b9bd-94f947487aa0"
         limit={3}
       />
     </div>
